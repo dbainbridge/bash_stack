e4f4ebc620e29f160b74cc52c77c7d7ea40e829b
David Bainbridge <dbainbridge@mac.com>
2012-05-01 10:39:41 -0500

    Fix copying of default html files.

--- install.sh
+++ install.sh
@@ -13,10 +13,15 @@ INSTALL_LOCATION=$(echo "$INSTALL_LOCATION" | sed 's/\/$//g')
 #install lib directory if lib directory exists
 if [ -d ./lib ] ; then
 	echo "Installing to: $INSTALL_LOCATION"
+
+	# remove previous installation if it exists
 	rm -rf "$INSTALL_LOCATION"
 	mkdir -p "$INSTALL_LOCATION/lib"
+
 	cp -r ./lib/* "$INSTALL_LOCATION/lib"
 	cp ./bash_stack.sh "$INSTALL_LOCATION"
+	cp -r ./lib/default_html "$INSTALL_LOCATION/lib"
+	
 	echo '#!/bin/bash' > "$INSTALL_LOCATION/tmp.tmp.sh"
 	echo "BASH_STACK_INSTALL_DIR=\"$INSTALL_LOCATION\"" >> "$INSTALL_LOCATION/tmp.tmp.sh"
 	cat "$INSTALL_LOCATION/bash_stack.sh"  >> "$INSTALL_LOCATION/tmp.tmp.sh"
