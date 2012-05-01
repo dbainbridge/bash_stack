340e8392152175f58b44ba29d623320660b1c3d5
David Bainbridge <dbainbridge@mac.com>
2012-05-01 10:38:54 -0500

    Changed nginx paths to more standardized locations.

--- lib/constants.sh
+++ lib/constants.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 export RUBY_PREFIX="/usr/local/ruby"
-export NGINX_VER="1.0.11"
-export NGINX_PREFIX="/srv/www"
-export NGINX_SBIN_PATH="/usr/local/sbin/nginx"
-export NGINX_CONF_PATH="/etc/nginx"
+
+#nginx settings
+export NGINX_VER="1.0.20"
+export NGINX_PREFIX="/opt/nginx"
+export NGINX_SBIN_PATH="$NGINX_PREFIX/sbin/nginx"
+export NGINX_CONF_PATH="$NGINX_PREFIX/conf"
 export NGINX_PID_PATH="/var/run/nginx.pid"
-export NGINX_ERROR_LOG_PATH="/srv/www/nginx_logs/error.log"
-export NGINX_HTTP_LOG_PATH="/srv/www/nginx_logs"
+export NGINX_ERROR_LOG_PATH="/var/log/nginx/error.log"
+export NGINX_HTTP_LOG_PATH="/var/log/nginx"
+
 export LOGRO_FREQ="monthly"
 export LOGRO_ROTA="12"
 
