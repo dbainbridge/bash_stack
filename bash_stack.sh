c0734e4f96b119c05423a47ad51df316ce5f3ebf
David Bainbridge <dbainbridge@mac.com>
2012-04-30 23:18:14 -0500

    Update for new html directory.

--- bash_stack.sh
+++ bash_stack.sh
@@ -1,8 +1,8 @@
 
 export BASH_STACK_INSTALL_DIR
 
-# source all files in this directory except this script
- for f in $BASH_STACK_INSTALL_DIR/lib/*; 
+# source all files in this directory
+ for f in $BASH_STACK_INSTALL_DIR/lib/*.sh; 
  do
  	echo $f
 	source "$f"
